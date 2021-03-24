8300b75254e95c280521e8c1e3211d4fef73f106
Kasady <44520511+buz71@users.noreply.github.com>
2021-03-24 21:53:34 +0300

    Constructor Lesson 1

--- Intro/Intro/Source.cpp
+++ Intro/Intro/Source.cpp
@@ -4,42 +4,69 @@ using std::cin;
 using std::endl;
 
 #define tab "\t"
+//#define INTRO
 
-class Point 
-{	
+class Point
+{
+	double x;
+	double y;
 
-		double x;
-		double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
 
-	public:
-		double get_x()const
-		{
-			return x;
-		}
+	double get_y()const
+	{
+		return y;
+	}
 
-		double get_y()const
-		{
-			return y;
-		}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
 
-		void set_x(double x)
-		{
-			this->x = x;
-		}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 
-		void set_y(double y)
-		{
-			this->y = y;
-		}
+	// Constructors
 
+	
+
+	Point() 
+	{
+		x = y = 0;
+		cout<< "Constructor:\t" << this << endl;
+	};
+
+	Point(double x, double y) 
+	{
+		this -> x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	// Methods
+
+	void print()const
+	{
+		cout << "X= " << x << tab << "Y= " << y << endl;
+	}
 };
-
-
 
 void main()
 {
 	setlocale(LC_ALL, "RU");
 
+#ifdef INTRO
 	Point A;
 
 	A.set_x(2);
@@ -52,8 +79,16 @@ void main()
 	cout << pA->get_x() << tab << pA->get_y() << endl;
 	//cout << *(pA).x << tab << *(pA).y << endl;
 
-
 	cout << sizeof(A) << endl;
 	cout << sizeof(pA);
+#endif // INTRO
+
+	Point A;
+
+	A.print();
+
+	Point B(2,3);
+
+	B.print();
 
 }
