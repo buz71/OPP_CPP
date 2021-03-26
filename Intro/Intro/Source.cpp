22b43a309f565d9d87baa4ff56e945a8120a0499
Kasady <44520511+buz71@users.noreply.github.com>
2021-03-26 22:04:06 +0300

    Constructor Lesson 2

--- Intro/Intro/Source.cpp
+++ Intro/Intro/Source.cpp
@@ -7,10 +7,21 @@ using std::endl;
 #define tab "\t"
 //#define INTRO
 #define DELIMITER "--------------------------------------\n"
-#define HOMEWORK
+//#define HOMEWORK
+//#define CONSTRUCTORS_CHECK
+
+//double Distance(Point A, Point B)
+//{
+//	int x = (A.x - B.x);
+//	int y = (A.y - B.y);
+//	return sqrt((double)(pow(x, 2) + pow(y, 2)));
+//
+//}
 
 class Point
 {
+
+private:
 	double x;
 	double y;
 
@@ -19,17 +30,14 @@ public:
 	{
 		return x;
 	}
-
 	double get_y()const
 	{
 		return y;
 	}
-
 	void set_x(double x)
 	{
 		this->x = x;
 	}
-
 	void set_y(double y)
 	{
 		this->y = y;
@@ -37,40 +45,58 @@ public:
 
 	// Constructors
 
-	
+	//Point() 
+	//{
+	//	x = y = 0;
+	//	cout<< "Constructor:\t" << this << endl;
+	//};
+	//Point(double x)
+	//{
+	//	this->x = x;
+	//	this->y = 0;
+	//	cout << "SingleArgumentConstructor: " << this << endl;
+	//	//Конструктор с одним параметром отвечает за преобразование типов.
+	//}
 
-	Point() 
-	{
-		x = y = 0;
-		//cout<< "Constructor:\t" << this << endl;
-	};
-
-	Point(double x, double y) 
+	Point(double x=0, double y=0) 
 	{
 		this -> x = x;
-		this->y = y;
-		//cout << "Constructor:\t" << this << endl;
+		this-> y = y;
+		cout << "Constructor:\t" << this << endl;
 	}
-
+	Point (const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:" << this << endl;
+	}
 	~Point()
 	{
-		//cout << "Destructor:\t" << this << endl;
+		cout << "Destructor:\t" << this << endl;
 	}
 
-	// Methods
+	//	Operators:
 
-	void print()const
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" <<this << endl;
+		return *this;
+	}
+
+	//	Methods:
+
+	void Print()const
 	{
 		cout << "X= " << x << tab << "Y= " << y << endl;
 	}
-
 	double Distance()
 	{
 		
 		return sqrt((double)(pow(this->x, 2) + pow(this->y, 2)));
 
 	}
-
 	double DistBetween(Point A, Point B)
 	{
 		int x = (A.x-B.x);
@@ -129,7 +155,35 @@ void main()
 
 #endif // HOMEWORK
 
+#ifdef CONSTRUCTORS_CHECK
+	int a;
+	Point A;
+	A.Print();
+	Point B(2, 3);
+	B.Print();
 
+	Point C = 5; // Single argument constructor
+	C.Print();
+	Point D(8);// Single argument constructor
+	D.Print();
+	int b(3); // Якобы вызыватся конструктор с одним параметром
+	int();	//Якобы вызывается конструктор по умолчанию для типа int
+			//и возвращает значение по умолчанию для типа int
+			//так можно получить значение по умолчанию для любого типа.
+
+	Point E{ 13 }; //Single argument constructor
+	E.Print();
+	Point F = B;
+	F.Print();
+	Point G;
+	G = F; // CopyAssigment  
+#endif // CONSTRUCTORS_CHECK
+
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point();
 
 
 
