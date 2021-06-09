22246734f07f31701a6091d3af5ca15c6e8ff6c7
Kasady <44520511+buz71@users.noreply.github.com>
2021-06-09 22:17:47 +0300

    List reverse_print DONE

--- STL/Lists/Main.cpp
+++ STL/Lists/Main.cpp
@@ -5,6 +5,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 #define tab "\t"
+//#define LIST_INSERT
 
 template<typename T>void list_properties(const std::list<T>& lst)
 {
@@ -18,6 +19,15 @@ template<typename T>void print_list(const std::list<T>& lst)
 	{
 		cout << *it << tab;
 	}
+	cout << endl;
+}
+template<typename T>void reverse_print_list(const std::list<T>& lst)
+{
+	for (std::list<int>::const_reverse_iterator it = lst.rbegin(); it != lst.rend(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
 }
 
 
@@ -27,10 +37,21 @@ void main()
 	std::list<int> lst = {3,5,8,13,21,};
 	list_properties(lst);
 	print_list(lst);
-	cout << endl;
+#ifdef LIST_INSERT
 	lst.push_front(2);
 	lst.push_back(34);
 	print_list(lst);
+	cout << endl;
 	list_properties(lst);
+	int index, value;
+	cout << "¬ведите индекс: "; cin >> index;
+	cout << "¬ведите значение: "; cin >> value; */
+		std::list<int>::iterator position = lst.begin();
+	for (int i = 0; i < index; i++)position++;
+	lst.insert(position, value);
+	print_list(lst);
+	cout << endl;
+#endif // LIST_INSERT
+	reverse_print_list(lst);
 
 }
