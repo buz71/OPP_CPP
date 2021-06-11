4401a3de4a3488f644e216bd7d8c0fac75c62371
Kasady <pupcok@yandex.ru>
2021-06-11 10:44:45 +0300

    GAI initial commit

---
+++ STL/GAI_ControlSystem/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include<list>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t";
+#define DELIMITER "----------------------------------------------\n";
+
+void main()
+{
+	setlocale(LC_ALL, "RU");
+	std::map<std::string, std::list<std::string>> Base;
+}
