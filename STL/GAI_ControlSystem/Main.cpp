22b19040b8bf436b5c96247b7a7e7689071ace5f
Kasady <44520511+buz71@users.noreply.github.com>
2021-06-12 07:19:18 +0300

    PrintByNumber DONE

--- STL/GAI_ControlSystem/Main.cpp
+++ STL/GAI_ControlSystem/Main.cpp
@@ -4,13 +4,14 @@
 using std::cin;
 using std::cout;
 using std::endl;
+typedef unsigned int uint;
 
 #define tab "\t";
-#define DELIMITER "----------------------------------------------\n";
+#define DELIMITER "---------------------------------------------------------------------------------------------\n";
 
-void PrintBase(std::map<int, std::list<std::string>> base) 
+void PrintBase(std::map<uint, std::list<std::string>> base) 
 {
-	for (std::map<int, std::list<std::string>>::iterator it_base = base.begin(); it_base != base.end(); it_base++)
+	for (std::map<uint, std::list<std::string>>::iterator it_base = base.begin(); it_base != base.end(); it_base++)
 	{
 		cout << "|Номер: " << it_base->first << "| - " << "|Нарушения: ";
 		for (std::list<std::string>::iterator it_list = it_base->second.begin(); it_list != it_base->second.end(); it_list++)
@@ -23,13 +24,34 @@ void PrintBase(std::map<int, std::list<std::string>> base)
 	}	
 	
 };
+void PrintByNumber(std::map<uint,std::list<std::string>> base, uint number)
+{
+	for (std::map<uint, std::list<std::string>>::iterator it_base = base.begin(); it_base != base.end(); it_base++)
+	{
+		if (it_base->first == number)
+		{
+			cout << "|Номер: " << it_base->first << "| - " << "|Нарушения: ";
+			for (std::list<std::string>::iterator it_list = it_base->second.begin(); it_list != it_base->second.end(); it_list++)
+			{
+				cout << "|" << it_list->data() << "|";
+			}
+			cout << "  |" << endl;
+			return;
+		}
+	}
+
+	cout << "Номера нет в базе!";
+}
 
 void main()
 {
 	setlocale(LC_ALL, "RU");
-	std::map<int, std::list<std::string>> Base;
-	Base.insert(std::pair<int, std::list<std::string>>(0, { "Превышение", "Угон" }));
-	Base.insert(std::pair<int, std::list<std::string>>(2, { "Превышение", "Угон" }));
-	Base.insert(std::pair<int, std::list<std::string>>(1, { "Превышение", "Угон" }));
-	PrintBase(Base);
+	std::map<uint, std::list<std::string>> Base;
+	Base.insert(std::pair<uint, std::list<std::string>>(1, { "Превышение", "Неправильная парковка" }));
+	Base.insert(std::pair<uint, std::list<std::string>>(2, { "Просроченная страховка", "Езда без документов" }));
+	Base.insert(std::pair<uint, std::list<std::string>>(3, { "Обгон на сплошной", "Авария" }));
+	//PrintBase(Base);
+	int number;
+	cout << "Введите номер для поиска в базе: "; cin >> number;
+	PrintByNumber(Base,number);
 }
